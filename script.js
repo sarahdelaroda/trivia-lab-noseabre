0b0ff464a85d236d404e3936fac8f1148d7c98bb
mvejarp <marcelavejarpalavecino@gmail.com>
2023-08-22 12:54:52 -0400

    estructura básica trivia

---
+++ script.js
@@ -0,0 +1,17 @@
+var nombreJugador=document.getElementById('nombreJugador');
+var boton=document.getElementById('btn--remix');
+
+boton.addEventListener('click',function(){
+    alert(`Bienvenida ${nombreJugador}`);
+})
+
+
+
+
+
+
+
+
+
+
+
