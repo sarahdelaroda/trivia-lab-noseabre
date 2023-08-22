5d3c05dd2818b5ea86b507a7ff92e50c69558e79
mvejarp <marcelavejarpalavecino@gmail.com>
2023-08-22 13:25:52 -0400

    corrección captura del input

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-var nombreJugador=document.getElementById('nombreJugador');
 var boton=document.getElementById('btn--remix');
 
+
+
 boton.addEventListener('click',function(){
+    var nombreJugador=document.getElementById('nombreJugador').value;
     alert(`Bienvenida ${nombreJugador}`);
 })
-
 
 
 
