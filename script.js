9f99c04b713f295ac3077a0abe9bc31cd559f25f
Sarah Rodriguez <77365776+nallyzee@users.noreply.github.com>
2023-08-22 16:30:45 -0600

    Correccion pendiente

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 var boton=document.getElementById('btn--remix');
 
-
-
 boton.addEventListener('click',function(){
     var nombreJugador=document.getElementById('nombreJugador').value;
     alert(`Bienvenida ${nombreJugador}`);
